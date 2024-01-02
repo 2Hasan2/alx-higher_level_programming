b0caa2dd3200bf9bc40cb0b44cc3a6bf9978fdef
2Hasan2 <h01554176846@gmail.com>
2024-01-02 22:06:16 +0200

    2024-01-02 10:06:16 م

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-numbers = [f'{i:02d}' for i in range(100)]
-result = ', '.join(numbers)
-print(result)
+for i in range(0, 100):
+    if i < 99:
+        print('{:02d}, '.format(i), end='')
+    else:
+        print('{:02d}'.format(i))
