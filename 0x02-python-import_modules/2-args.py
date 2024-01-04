5252fa02d95c7de6888f2818f9aca745fb23693e
2Hasan2 <h01554176846@gmail.com>
2024-01-04 19:51:59 +0200

    2024-01-04 07:51:59 م

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args_count = len(sys.argv) - 1
-    print(f"{args_count} argument{'s' if args_count != 1 else ''}:")
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
+    args = sys.argv[1:]
+    handle_lang = 's' if len(args) != 1 else ''
+    handle_lang += ':' if len(args) else '.'
+    print(f"{len(args)} argument{handle_lang}")
+    for i, arg in enumerate(args):
+        print(f"{i + 1}: {arg}")
