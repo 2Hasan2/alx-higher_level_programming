1da8408770ef4d9886011d3111194f2512ffd8ac
2Hasan2 <h01554176846@gmail.com>
2024-01-04 17:07:27 +0200

    2024-01-04 05:07:27 م

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as HN
+    import add_0 as meme
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, HN.add(a, b)))
+    print("{} + {} = {}".format(a, b, meme.add(a, b)))
 
