ee2f1bf7e0ff33109055f10a858b098d825e359d
2Hasan2 <h01554176846@gmail.com>
2024-01-04 18:33:20 +0200

    2024-01-04 06:33:20 م

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    try:
-        a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
-    except ValueError:
-        print("Invalid input. Please provide integer values for <a> and <b>.")
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    from calculator_1 import add, sub, mul, div
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    try:
-        result = {
-            '+': add(a, b),
-            '-': sub(a, b),
-            '*': mul(a, b),
-            '/': div(a, b)
-        }.get(operator, "Unknown operator. Available operators: +, -, * and /")
-
-        print("{} {} {} = {}".format(a, operator, b, result))
-    except ZeroDivisionError:
-        print("Error: Division by zero is not allowed.")
-        sys.exit(1)
+    print("{} {} {} = {}".format(a, operator, b, result))
