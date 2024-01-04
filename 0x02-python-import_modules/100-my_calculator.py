0cd8bfd11d16244106c3144355af0b95fc0ac935
2Hasan2 <h01554176846@gmail.com>
2024-01-04 18:22:21 +0200

    2024-01-04 06:22:21 م

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
