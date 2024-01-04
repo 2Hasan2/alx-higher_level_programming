af52fcaf7d9c12483dbc0ab66119b96ac3cba497
2Hasan2 <h01554176846@gmail.com>
2024-01-04 18:21:19 +0200

    2024-01-04 06:21:19 م

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    try:
+        a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+    except ValueError:
+        print("Invalid input. Please provide integer values for <a> and <b>.")
+        sys.exit(1)
+
+    try:
+        result = {
+            '+': add(a, b),
+            '-': sub(a, b),
+            '*': mul(a, b),
+            '/': div(a, b)
+        }.get(operator, "Unknown operator. Available operators: +, -, * and /")
+
+        print("{} {} {} = {}".format(a, operator, b, result))
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+        sys.exit(1)
