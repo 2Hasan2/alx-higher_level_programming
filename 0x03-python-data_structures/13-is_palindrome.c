31bd06d367c132530880a80e68e592bf6f91929c
2Hasan2 <h01554176846@gmail.com>
2024-01-06 20:55:56 +0200

    fix betty

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-
-
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to head of list
@@ -8,26 +6,27 @@
  * An empty list is considered a palindrome
  */
 
- int is_palindrome(listint_t **head){
-		listint_t *current = *head;
-		int i = 0, j = 0, k = 0;
-		int array[10000];
-		if (head == NULL)
-			return (1);
-		while (current != NULL)
-		{
-			array[i] = current->n;
-			current = current->next;
-			i++;
-		}
-		j = i - 1;
-		while (k < j)
-		{
-			if (array[k] != array[j])
-				return (0);
-			k++;
-			j--;
-		}
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+	int i = 0, j = 0, k = 0;
+	int array[10000];
+
+	if (head == NULL)
 		return (1);
- }
- 
+	while (current != NULL)
+	{
+		array[i] = current->n;
+		current = current->next;
+		i++;
+	}
+	j = i - 1;
+	while (k < j)
+	{
+		if (array[k] != array[j])
+			return (0);
+		k++;
+		j--;
+	}
+	return (1);
+}
