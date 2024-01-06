39fc5aec7307f98739594a0e40b37e5ee02d9bf1
2Hasan2 <h01554176846@gmail.com>
2024-01-06 20:36:53 +0200

    2024-01-06 08:36:53 م

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = "".join([ele for ele in my_string if ele.lower() != 'c'])
+    new_string = ""
+    for ele in my_string:
+        if ele not in ["c","C"]:
+            new_string += ele
     return new_string
-
-no_c("Best School")
