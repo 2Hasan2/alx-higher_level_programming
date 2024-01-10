05ca8028ac239ebb6dd33c1a1816fcfe48c3b7d6
2Hasan2 <h01554176846@gmail.com>
2024-01-10 14:38:39 +0200

    3-done

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    print([ele for ele in set_1 if set_2.include(ele)])
-
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-
-common_elements(set_1,set_2)
+    return [ele for ele in set_1 if ele in set_2]
