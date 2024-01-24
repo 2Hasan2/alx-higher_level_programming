9707fc201148ec9ac05343120286ab04efcccac8
2Hasan2 <h01554176846@gmail.com>
2024-01-24 22:33:43 +0200

    solve task #5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Square class with size attribute"""
+
+
+class Square:
+	"""Square class with size attribute"""
+	def __init__(self, size=0):
+		"""Initializes Square with size attribute"""
+		self.__size = size
+
+	def area(self):
+		"""Returns area of Square instance"""
+		return self.__size ** 2
+
+	@property
+	def size(self):
+		"""Getter for size attribute"""
+		return self.__size
+
+	@size.setter
+	def size(self, value):
+		"""Setter for size attribute"""
+		if type(value) != int:
+			raise TypeError("size must be an integer")
+		elif value < 0:
+			raise ValueError("size must be >=0")
+		else:
+			self.__size = value
