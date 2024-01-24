be04ef043fcaefb4824053b084b2dc553cee98b7
2Hasan2 <h01554176846@gmail.com>
2024-01-24 22:30:52 +0200

    solve task #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class with size attribute"""
+
+
+class Square:
+    """Square class with size attribute"""
+    def __init__(self, size):
+        """Initializes Square with size attribute"""
+        self.__size = size
