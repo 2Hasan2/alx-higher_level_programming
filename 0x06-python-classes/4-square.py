8929d7a0a30d2c182334b0a5defe3a961da1fcfd
2Hasan2 <h01554176846@gmail.com>
2024-01-24 22:35:05 +0200

    fix tabs

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,26 +4,26 @@
 
 
 class Square:
-	"""Square class with size attribute"""
-	def __init__(self, size=0):
-		"""Initializes Square with size attribute"""
-		self.__size = size
+    """Square class with size attribute"""
+    def __init__(self, size=0):
+        """Initializes Square with size attribute"""
+        self.__size = size
 
-	def area(self):
-		"""Returns area of Square instance"""
-		return self.__size ** 2
+    def area(self):
+        """Returns area of Square instance"""
+        return self.__size ** 2
 
-	@property
-	def size(self):
-		"""Getter method for size"""
-		return self.__size
+    @property
+    def size(self):
+        """Getter method for size"""
+        return self.__size
 
-	@size.setter
-	def size(self, value):
-		"""Setter method for size"""
-		if type(value) != int:
-			raise TypeError("size must be an integer")
-		elif value < 0:
-			raise ValueError("size must be >=0")
-		else:
-			self.__size = value
+    @size.setter
+    def size(self, value):
+        """Setter method for size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = value
