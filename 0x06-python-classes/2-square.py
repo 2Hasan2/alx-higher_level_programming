03273cdcd008b2110e0387a62d688ed8a1360303
2Hasan2 <h01554176846@gmail.com>
2024-01-24 22:32:16 +0200

    solve task #2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,12 +3,12 @@
 
 
 class Square:
-	"""Square class with size attribute"""
-	def __init__(self, size=0):
-		"""Initializes Square with size attribute"""
-		if type(size) != int:
-			raise TypeError("size must be an integer")
-		elif size < 0:
-			raise ValueError("size must be >=0")
-		else:
-			self.__size = size
+    """Square class with size attribute"""
+    def __init__(self, size=0):
+        """Initializes Square with size attribute"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
