4d362706ecda602e749df0cac30f7eca54fc383c
2Hasan2 <h01554176846@gmail.com>
2024-02-05 20:16:50 +0200

    sone

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""MyList module"""
+
+class MyList(list):
+    """MyList class"""
+
+    def __init__(self):
+        """initialization"""
+        super().__init__()
+
+    def print_sorted(self):
+        """print sorted list"""
+        print(sorted(self))
