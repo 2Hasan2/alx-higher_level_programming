374834ff98510e11d06680a7b487562414e3c959
2Hasan2 <h01554176846@gmail.com>
2024-02-05 20:06:31 +0200

    easy :)

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+
+
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+
 def lookup(obj):
-    return dir(obj)
+	 return dir(obj)
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
