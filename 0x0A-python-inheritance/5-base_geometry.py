63c7457c25f09f8b2c6dae85f131569167262f41
2Hasan2 <h01554176846@gmail.com>
2024-02-05 20:30:17 +0200

    eaaaaaaasy

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Module to create a class """
+
+class BaseGeometry:
+    """ Class BaseGeometry """
+    def area(self):
+        """ Method to calculate the area """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """ Method to validate the value """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
