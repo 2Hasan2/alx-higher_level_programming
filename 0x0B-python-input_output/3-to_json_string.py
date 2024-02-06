ebd4917764fa6adfeedfb3c8934ad8f27f7cddfb
2Hasan2 <h01554176846@gmail.com>
2024-02-06 22:03:08 +0200

    5 done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""To JSON string"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """To JSON string"""
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""From JSON string"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """From JSON string"""
+    return json.loads(my_str)
