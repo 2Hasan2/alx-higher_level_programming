6b875723dc02823057f64adca6b1396551104ef4
2Hasan2 <h01554176846@gmail.com>
2024-02-06 22:10:24 +0200

    11  done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""Student to JSON"""
+
+
+class Student:
+    """Student to JSON"""
+
+    def __init__(self, first_name, last_name, age):
+        """Student to JSON"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Student to JSON"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        """Reload from JSON"""
+        for k, v in json.items():
+            setattr(self, k, v)
+
