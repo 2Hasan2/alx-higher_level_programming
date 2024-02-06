5bc3ae2cb2ac5c7fcb945516dbdffc3bfa5fce54
2Hasan2 <h01554176846@gmail.com>
2024-02-06 22:13:47 +0200

    fix style

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,7 +15,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """Student to JSON"""
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if (type(attrs) is list and
+                all(type(ele) is str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     my_list = load_from_json_file('add_item.json')
-except:
+except FileNotFoundError:
     my_list = []
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
