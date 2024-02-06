78bfaba6181d92ffa2d5a972a5533b2eab06e9de
2Hasan2 <h01554176846@gmail.com>
2024-02-06 21:59:28 +0200

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write file"""
+
+
+def write_file(filename="", text=""):
+    """Write file"""
+    with open(filename , mode='w', encoding='utf-8') as f:
+        return f.write(text)
