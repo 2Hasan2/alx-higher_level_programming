2a3718f9d6af4234eebcbf9cdb2a1ea6ddfe6fda
2Hasan2 <h01554176846@gmail.com>
2024-02-06 22:18:25 +0200

    2024-02-06 10:18:25 PM

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,14 +9,13 @@ def pascal_triangle(n):
     triangle = [[1]]
 
     for i in range(1, n):
-        row = [1]  # First element of each row is always 1
+        row = [1]
 
-        # Calculate the middle elements of the row
         for j in range(1, i):
             element = triangle[i - 1][j - 1] + triangle[i - 1][j]
             row.append(element)
 
-        row.append(1)  # Last element of each row is always 1
+        row.append(1)
         triangle.append(row)
 
     return triangle
