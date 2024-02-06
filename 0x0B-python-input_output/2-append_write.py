ec3aa70bf6e4dd50280a9fca9a99448f6805fc94
2Hasan2 <h01554176846@gmail.com>
2024-02-06 22:01:02 +0200

    3 done

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Append to a file"""
+
+
+def append_write(filename="", text=""):
+    """Append to a file"""
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
