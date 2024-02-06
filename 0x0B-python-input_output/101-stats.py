ba79227c91e5e8ba66ffb361d239049adf86a4b9
2Hasan2 <h01554176846@gmail.com>
2024-02-06 22:26:13 +0200

    101 done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,33 +1,49 @@
 #!/usr/bin/python3
 
-"""Stats"""
+"""101-stats.py"""
 
 
-def print_stats(file_size, status_codes):
+from collections import defaultdict
+
+
+def print_stats(size, status_codes):
     """Print stats"""
-    print("File size: {:d}".format(file_size))
-    for k, v in sorted(status_codes.items()):
-        print("{:s}: {:d}".format(k, v))
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
     import sys
 
-    file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
+    size = 0
+    status_codes = defaultdict(int)
+    valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
     count = 0
+
     try:
         for line in sys.stdin:
-            count += 1
-            tokens = line.split()
-            file_size += int(tokens[-1])
-            if tokens[-2] in status_codes:
-                status_codes[tokens[-2]] += 1
             if count == 10:
-                print_stats(file_size, status_codes)
-                count = 0
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
     except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
+        print_stats(size, status_codes)
         raise
-    print_stats(file_size, status_codes)
