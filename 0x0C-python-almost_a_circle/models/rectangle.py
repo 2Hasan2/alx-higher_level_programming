89424d62cc0108f98409223e77cec3a23eae8530
2Hasan2 <h01554176846@gmail.com>
2024-02-12 20:47:41 +0200

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,4 +4,52 @@ from models.base import Base
 
 
 class Rectangle(Base):
-	pass
+    """ Rectangle class."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle."""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """Get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the Rectangle."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the Rectangle."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x of the Rectangle."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y of the Rectangle."""
+        self.__y = value
