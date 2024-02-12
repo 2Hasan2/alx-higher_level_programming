e9b95bec636925e6e599a44b0d65b98e812bec19
2Hasan2 <h01554176846@gmail.com>
2024-02-12 21:33:51 +0200

    3. Validate attributes

---
+++ 0x0C-python-almost_a_circle/models/a.rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Rectangle module."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle class."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle."""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the Rectangle."""
+        self.integer_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the Rectangle."""
+        self.integer_validator("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x of the Rectangle."""
+        self.integer_validator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y of the Rectangle."""
+        self.integer_validator("y", value)
+        self.__y = value
+
+    # Errors Handling
+    def integer_validator(self, name, value):
+        """Validate the value."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if name in ("width", "height") and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        if name in ("x", "y") and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,69 +1,113 @@
 #!/usr/bin/python3
-"""Rectangle module."""
+"""this defines a class named rectangle"""
+
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ Rectangle class."""
-
-    def __init__(self, width, height, x=0, y=0, id=None):
+    """class Rectangle implements Base"""
+    def _init_(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle."""
+        super()._init_(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
-        """Get the width of the Rectangle."""
+        """adding width the getter for width"""
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """Set the width of the Rectangle."""
-        self.integer_validator("width", value)
-        self.__width = value
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = width
 
     @property
     def height(self):
-        """Get the height of the Rectangle."""
+        """getter for the height"""
         return self.__height
 
     @height.setter
-    def height(self, value):
-        """Set the height of the Rectangle."""
-        self.integer_validator("height", value)
-        self.__height = value
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("heigth must be > 0")
+
+        self.__height = height
 
     @property
     def x(self):
-        """Get the x of the Rectangle."""
+        "getter for x"
         return self.__x
 
     @x.setter
-    def x(self, value):
-        """Set the x of the Rectangle."""
-        self.integer_validator("x", value)
-        self.__x = value
+    def x(self, x):
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = x
 
     @property
     def y(self):
-        """Get the y of the Rectangle."""
+        """getter for y"""
         return self.__y
 
     @y.setter
-    def y(self, value):
-        """Set the y of the Rectangle."""
-        self.integer_validator("y", value)
-        self.__y = value
+    def y(self, y):
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
 
-    # Errors Handling
-    def integer_validator(self, name, value):
-        """Validate the value."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if name in ("width", "height") and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        if name in ("x", "y") and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
+        self.__y = y
+
+    def area(self):
+        """returns the area value"""
+        return (self._width * self._height)
+
+    def display(self):
+        """dispaly the rectangle in stdout"""
+        if self.__y != 0:
+            print("\n" * self.__y, end="")
+        for height in range(0, self.__height):
+            print(" " * self.__x, end="")
+            for width in range(0, self.__width):
+                print("#", end="")
+            print("")
+
+    def _str_(self):
+        """returns  task 6"""
+        return (
+                f"[{type(self)._name}] ({self.id}) {self.x}/{self._y}"
+                f" - {self._width}/{self._height}"
+                )
+
+    def update(self, *args, **kwargs):
+        """adding the new values to variables"""
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                self._setattr_(key, value)
+            return
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
+
+    def to_dictionary(self):
+        """returns dicitionary represention"""
+        return {'x': self._x, 'y': self._y, 'id': self.id,
+                'height': self._height, 'width': self._width}
