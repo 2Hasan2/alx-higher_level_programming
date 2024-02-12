4f02d018cd3be7676fde4068b050069a08713501
2Hasan2 <h01554176846@gmail.com>
2024-02-12 20:15:06 +0200

    1.Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
 
 
 class Base:
+    """Base model."""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base."""
         if id is not None:
             self.id = id
         else:
