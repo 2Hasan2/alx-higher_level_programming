616829c07b4493df94cee4bfff140a45b6ae9c51
2Hasan2 <h01554176846@gmail.com>
2024-02-12 21:41:11 +0200

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,13 +7,13 @@ from models.base import Base
 
 class Rectangle(Base):
     """class Rectangle implements Base"""
-    def _init_(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle."""
-        super()._init_(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
