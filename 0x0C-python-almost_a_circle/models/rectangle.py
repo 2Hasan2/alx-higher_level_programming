be8de17a4ee544249f8c9d94af61e1c5cc71a71c
2Hasan2 <h01554176846@gmail.com>
2024-02-12 20:57:09 +0200

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set the width of the Rectangle."""
+        self.__is_validate_positive_integer(value, "width")
         self.__width = value
 
     @property
@@ -32,6 +33,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set the height of the Rectangle."""
+        self.__is_validate_positive_integer(value, "width")
         self.__height = value
 
     @property
@@ -42,6 +44,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set the x of the Rectangle."""
+        self.__is_validate_positive_integer_or_zero(value, "x")
         self.__x = value
 
     @property
@@ -52,4 +55,23 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Set the y of the Rectangle."""
+        self.__is_validate_positive_integer_or_zero(value, "y")
         self.__y = value
+
+    # Errors Handling
+    def __is_validate_integer(self, value, attribute):
+        """Validate if value is an integer."""
+        if not isinstance(value, int):
+            raise TypeError(f"{attribute} must be an integer")
+
+    def __is_validate_positive_integer(self, value, attribute):
+        """Validate if value is a positive integer."""
+        self.__is_validate_integer(value, attribute)
+        if value <= 0:
+            raise ValueError(f"{attribute} must be > 0")
+
+    def __is_validate_positive_integer_or_zero(self, value, attribute):
+        """Validate if value is a positive integer or zero."""
+        self.__is_validate_integer(value, attribute)
+        if value < 0:
+            raise ValueError(f"{attribute} must be >= 0")
