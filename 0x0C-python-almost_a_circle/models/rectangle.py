d56d7c2a6d0c772a749a5c074aab0b902b2d2c27
2Hasan2 <h01554176846@gmail.com>
2024-02-12 22:33:50 +0200

    2024-02-12 10:33:50 PM

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,7 +74,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args,  **kwargs):
         """Update the attributes of the Rectangle."""
         if len(args) >= 1:
             self.id = args[0]
