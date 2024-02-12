798874d8e8e9150d6db35b9c3848a23a3341f3dd
2Hasan2 <h01554176846@gmail.com>
2024-02-12 23:12:17 +0200

    2024-02-12 11:12:17 PM

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -52,6 +52,3 @@ class Square(Rectangle):
     def to_dictionary(self):
         """Return the dictionary representation of a Square."""
         return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
-
-    def display(self):
-        return super().display()
