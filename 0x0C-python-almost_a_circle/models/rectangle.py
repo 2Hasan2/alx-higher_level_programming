130b7ffeee8befbca7d3df7ec316f6ce712aad96
2Hasan2 <h01554176846@gmail.com>
2024-02-12 21:14:27 +0200

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,60 +8,63 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle."""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+
 
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
+        """Get the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """Set the width of the Rectangle."""
+        self.integer_validator("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """Set/get the height of the Rectangle."""
+        """Get the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
+        """Set the height of the Rectangle."""
+        self.integer_validator("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """Set/get the x coordinate of the Rectangle."""
+        """Get the x of the Rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        """Set the x of the Rectangle."""
+        self.integer_validator("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """Set/get the y coordinate of the Rectangle."""
+        """Get the y of the Rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        """Set the y of the Rectangle."""
+        self.integer_validator("y", value)
         self.__y = value
+
+    # Errors Handling
+    def integer_validator(self, name, value):
+        """Validate the value."""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if name in ("width", "height") and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        if name in ("x", "y") and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
