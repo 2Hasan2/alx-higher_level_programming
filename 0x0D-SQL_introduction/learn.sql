5fb99cd72595df081b93a9adb9da342a7ec21624
2Hasan2 <h01554176846@gmail.com>
2024-04-16 10:09:24 +0200

    2024-04-16 10:09:24 ص

--- 0x0D-SQL_introduction/learn.sql
+++ 0x0D-SQL_introduction/learn.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS `learn`;
 
 USE learn;
 
-CREATE TABLE student (
+CREATE TABLE IF NOT EXISTS  student (
     student_id INT PRIMARY KEY,
     name VARCHAR(20),
     major VARCHAR(20)
