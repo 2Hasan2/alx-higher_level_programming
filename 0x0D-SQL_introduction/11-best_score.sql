6c1fce22e29c19ed449bef024722f301a2fbda3b
2Hasan2 <h01554176846@gmail.com>
2024-04-16 14:57:27 +0200

    2024-04-16 02:57:27 PM

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- list all databases in server
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++
@@ -1,4 +0,0 @@
--- List all records of the table second_table, ordered by score in descending order.    
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
