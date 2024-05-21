0e4ee9e2c56a9651817d03223f5ce20070b4d3e0
2Hasan2 <h01554176846@gmail.com>
2024-05-21 22:41:15 +0300

    done1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => console.log(data || err));
+fs.readFile(process.argv[2], 'utf8', (err, data) => console.log(err || data));
