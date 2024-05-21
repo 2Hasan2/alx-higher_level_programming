40c337404168c18812f3065a76b5fdbcd59461ef
2Hasan2 <h01554176846@gmail.com>
2024-05-21 23:02:15 +0300

    done3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const URL = process.argv[2];
 
 request(URL, (err, res) => {
-  console.log(err || `code: ${res.statusCode}`);
+	console.log(err || `code: ${res.statusCode}`);
 });
