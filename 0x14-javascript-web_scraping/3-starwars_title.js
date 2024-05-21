2914fae3d2bb7f086a953db7d01783a90e7b78f0
2Hasan2 <h01554176846@gmail.com>
2024-05-21 23:12:56 +0300

    done4

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+const URL = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+
+request(URL, (err, res) => {
+	console.log(err || JSON.parse(res.body).title);
+});
