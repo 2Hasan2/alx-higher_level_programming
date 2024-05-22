85f49d678eccbb9b12e48eaad95c16346385ad2e
Hasan Mohamed Hasan <133760904+2Hasan2@users.noreply.github.com>
2024-05-22 04:43:37 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,15 +3,24 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, _, body) => {
-  if (err) return console.error(err);
-
-  const tasks = JSON.parse(body);
-  const completed = tasks.filter(task => task.completed)
-                         .reduce((acc, task) => {
-                           acc[task.userId] = (acc[task.userId] || 0) + 1;
-                           return acc;
-                         }, {});
-  
-  console.log(completed);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
