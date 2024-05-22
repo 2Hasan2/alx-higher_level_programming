ce6aef6988cdb8e41819e06526e7e6ed64248c22
2Hasan2 <h01554176846@gmail.com>
2024-05-22 05:38:57 +0300

    done101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,16 +3,16 @@ const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 request(url, function (err, _, body) {
   if (!err) {
-    printCharacters(JSON.parse(body).characters, 0);
+    printChar(JSON.parse(body).characters, 0);
   }
 });
 
-function printCharacters (char, index) {
+function printChar (char, index) {
   request(char[index], function (error, _, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < char.length) {
-        printCharacters(char, index + 1);
+        printChar(char, index + 1);
       }
     }
   });
